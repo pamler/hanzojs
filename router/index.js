6e68a1f6fc8372d386d082ca7627b8e628cfa80d
葛彦斌 <pamler@126.com>
2017-07-14 15:10:11 +0800

    change router animation

--- router/index.js
+++ router/index.js
@@ -1,19 +1,23 @@
 import React, { PropTypes } from 'react'
-import { Dimensions, Animated, Easing, StyleSheet, View } from "react-native";
+import { render } from 'react-dom'
+import { Dimensions, Animated, Easing, StyleSheet, View, Image } from "react-native";
 import BrowserNavContainer from "./browser-nav-container";
 import ServerNavContainer from "./server-nav-container";
 import Navigation, { addNavigationHelpers } from 'react-navigation/lib/react-navigation.web.js'
 
 let reactAppContext = {};
+let onProcessing = null;
+let onProcessed = null;
 let isNodeRuntime = false
 let matchToRoute
 
+const isFunction = (handle) => (typeof handle === 'function');
 const AnimateValue = (v) => new Animated.Value(v);
 
 /**
  * 简单实现切换路由时页面切换，以后这里可以写个动画进行页面切换
  */
-class NavAnimateView extends React.Component {
+class NavAnimateView extends React.PureComponent {
 
   static propTypes = {
     route: PropTypes.string,
@@ -45,7 +49,7 @@ class NavAnimateView extends React.Component {
   * 获取进入页面动画样式
   */
   getPageInStyle(isForward) {
-    let animateStyle = { backfaceVisibility: 'hidden','willChange':'transform','perspective':1000 }
+    let animateStyle = { backfaceVisibility: 'hidden' }
     let transXInterpolate = this.translateX.interpolate({
       inputRange: [0, 1],
       outputRange: [this.endX, this.beginX]
@@ -54,14 +58,14 @@ class NavAnimateView extends React.Component {
     return animateStyle;
   }
 
-   /**
-    * 初始化进入页面动画样式
-    */
+  /**
+   * 初始化进入页面动画样式
+   */
   getPageOutStyle(state) {
-    let outStyle = { backfaceVisibility: 'hidden','willChange':'transform','perspective':1000 }
+    let outStyle = { backfaceVisibility: 'hidden' }
     let transZInterpolate = this.translateZ.interpolate({
       inputRange: [0, 1],
-      outputRange: [0,-300]
+      outputRange: [0, -300]
     });
     outStyle.transform = [{ translate3d: '0,0,0' }, { translateZ: transZInterpolate }];
     return outStyle;
@@ -73,18 +77,18 @@ class NavAnimateView extends React.Component {
   playAnimation() {
     this.createAnimations.getInAnimate().start();
     if (this.state.lastChildren) {
-      this.createAnimations.getOutAnimate().start(()=>{
-        this.setState({lastChildren:undefined})
+      this.createAnimations.getOutAnimate().start(() => {
+        this.setState({ lastChildren: undefined })
       });
     }
   }
 
   inAnimation() {
-    return Animated.timing(this.translateX, { toValue: 1, duration: 280, easing: Easing.linear });
+    return Animated.timing(this.translateX, { toValue: 1, duration: 260, easing: Easing.inOut(Easing.ease) });
   }
 
   outAnimation() {
-    return Animated.timing(this.translateZ, { toValue: 1, duration: 280, easing: Easing.linear });
+    return Animated.timing(this.translateZ, { toValue: 1, duration: 260, easing: Easing.inOut(Easing.ease) });
   }
 
   _shouldSetResponder() {
@@ -100,7 +104,7 @@ class NavAnimateView extends React.Component {
       this.setState({
         lastChildren: this.props.children,
         initStyle: this.getPageInStyle(),
-        outStyle:this.getPageOutStyle()
+        outStyle: this.getPageOutStyle()
       })
     }
   }
@@ -141,22 +145,219 @@ class NavAnimateView extends React.Component {
   }
 }
 
-const NavView = ({ navigation, router, isForward }) => {
-  const { state } = navigation;
-  const Component = router.getComponentForState(state);
-  const { path } = router.getPathAndParamsForState(state);
-  isForward = isForward || false;
-  return (
-    <NavAnimateView route={path} isForward={isForward}>
-      <Component
-        navigation={addNavigationHelpers({
-          ...navigation,
-          state: state.routes[state.index],
-        })}
-      />
-    </NavAnimateView>
-  );
-};
+
+class NavView extends React.Component {
+
+  /**
+   * 组件构造函数
+   * @param props {Object} 组件props
+   */
+  constructor(props) {
+    super(props);
+    this.routeComponent = this.props;
+    //设置默认state
+    this.state = {
+      //需要渲染的组件
+      requiredComponent: this.isAsyncComponent ? null : this.routeComponent
+    }
+  }
+
+  /**
+   * 当前路由需要展示的组件
+   */
+  get routeComponent() {
+    return this._routeComponent;
+  }
+
+  /**
+   * 设置当前路由需要展示的组件
+   */
+  set routeComponent(props) {
+    const { navigation, router } = props;
+    const { state } = navigation;
+    this._routeComponent = router.getComponentForState(state);
+  }
+
+  /**
+   * 判断当前props.asyncComponent 是否为一个异步加载组件
+   * 而不是纯粹的React组件
+   */
+  get isAsyncComponent() {
+    let asyncComponent = this.routeComponent;
+    return !(asyncComponent instanceof React.Component || this.isReduxConnect) && isFunction(asyncComponent)
+  }
+
+  /**
+   * 判断是否为一个redux的connect
+   */
+  get isReduxConnect() {
+    let asyncComponent = this.routeComponent;
+    return isFunction(asyncComponent) && (asyncComponent.prototype instanceof React.Component);
+  }
+
+  /**
+   * 组件第一次渲染完成时，执行一次异步获取
+   */
+  componentDidMount() {
+    this.asyncRequireComponent();
+  }
+
+  /**
+   * 当路由切换时
+   * @param nextProps {Object} 新的props
+   */
+  componentWillReceiveProps(nextProps) {
+    this.state.requiredComponent = null;
+    this.routeComponent = nextProps;
+    this.asyncRequireComponent();
+  }
+
+  /**
+   * 仅在requiredComponent变更时，才刷新组件内容
+   * @param nextProps {Object} 新的props
+   * @param nextState {Object} 新的state
+   * @returns {Boolean} 
+   */
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.requiredComponent && nextState.requiredComponent.prototype instanceof React.Component;
+  }
+
+  /**
+   * 尝试异步方式获取要显示的组件
+   */
+  asyncRequireComponent() {
+    if (this.isAsyncComponent) {
+      this.state.requiredComponent = null;
+      this.onProcessing();
+      //异步加载组件，然后进行渲染
+      this.routeComponent().then((m) => {
+        let requiredComponent = (m.default || m);
+        this.setState({ requiredComponent });
+        this.onProcessed();
+      }).catch((ex) => {
+        console.error(ex);
+      })
+    } else if (this.routeComponent !== this.state.requiredComponent) {
+      //如果传入的是同步组件
+      let requiredComponent = this.routeComponent;
+      this.setState({ requiredComponent });
+    }
+  }
+
+  /**
+   * 异步组件请求中，可以在次函数中添加loaing效果
+   */
+  onProcessing() {
+    if (isFunction(onProcessing)) {
+      onProcessing();
+    } else {
+      Processing.show();
+    }
+  }
+
+  /**
+   * 异步组件请求完毕
+   */
+  onProcessed() {
+    if (isFunction(onProcessed)) {
+      onProcessed();
+    } else {
+      Processing.hide();
+    }
+  }
+
+  /**
+   * 组件渲染
+   */
+  render() {
+    const Component = this.state.requiredComponent;
+    const { navigation, router, isForward } = this.props;
+    const state = navigation.state;
+    const { path } = router.getPathAndParamsForState(state);
+    const forward = isForward || false;
+    return (
+      <NavAnimateView route={path} isForward={forward}>
+        <Component
+          navigation={addNavigationHelpers({
+            ...navigation,
+            state: state.routes[state.index],
+          })}
+        />
+      </NavAnimateView>
+    );
+  }
+}
+
+class Processing {
+
+  static show() {
+    this.timer = setTimeout(() => this.preloader(), 200)
+  }
+
+  static preloader() {
+    let container = this.container = document.createElement('div');
+    container.innerHTML = `
+      <style type="text/css">
+        .preloader-indicator-overlay {
+            visibility: visible;
+            opacity: 0;
+            background: 0 0
+        }
+        .preloader-indicator-modal {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            background: rgba(0,0,0,.8);
+            margin-left:-30px;
+            margin-top:-30px;
+            z-index: 11000;
+            padding:10px;
+            border-radius: 4px;
+        }
+        .preloader-indicator-modal .preloader {
+            display: block;
+            width: 40px;
+            height: 40px;
+             -webkit-transform-origin: 50%;
+            transform-origin: 50%;
+            -webkit-animation: preloader-spin 1s steps(12,end) infinite;
+            animation: preloader-spin 1s steps(12,end) infinite
+        }
+        .preloader:after {
+            display: block;
+            content: "";
+            width: 100%;
+            height: 100%;
+            background-image:url("data:image/svg+xml;charset=utf-8,%3Csvg%20viewBox%3D'0%200%20120%20120'%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20xmlns%3Axlink%3D'http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink'%3E%3Cdefs%3E%3Cline%20id%3D'l'%20x1%3D'60'%20x2%3D'60'%20y1%3D'7'%20y2%3D'27'%20stroke%3D'%23fff'%20stroke-width%3D'11'%20stroke-linecap%3D'round'%2F%3E%3C%2Fdefs%3E%3Cg%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(30%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(60%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(90%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(120%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(150%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.37'%20transform%3D'rotate(180%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.46'%20transform%3D'rotate(210%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.56'%20transform%3D'rotate(240%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.66'%20transform%3D'rotate(270%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.75'%20transform%3D'rotate(300%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.85'%20transform%3D'rotate(330%2060%2C60)'%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E");
+            background-position: 50%;
+            background-size: 100%;
+            background-repeat: no-repeat
+        }
+        @-webkit-keyframes preloader-spin {
+            100% {
+                -webkit-transform: rotate(360deg)
+            }
+        }
+        @keyframes preloader-spin {
+            100% {
+                -webkit-transform: rotate(360deg);
+                transform: rotate(360deg)
+            }
+        }
+      </style>
+      <div class="preloader-indicator-overlay"></div>
+      <div class="preloader-indicator-modal"><span class="preloader preloader-white"></span></div>
+     `;
+    document.body.appendChild(container);
+  }
+
+  static hide() {
+    clearTimeout(this.timer);
+    if (this.container) {
+      this.container.parentElement.removeChild(this.container);
+    }
+  }
+}
 
 /**
  * 由于react-navigation 在web平台下只能使用TabRouter以及StackRouter 
@@ -183,11 +384,16 @@ Navigation.setConfig = function (config) {
   reactAppContext = config.reactAppContext
   isNodeRuntime = config.isNodeRuntime
   matchToRoute = config.matchToRoute
+  onProcessing = config.onProcessing;
+  onProcessed = config.onProcessed;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    transform: [{ translate3d: '0,0,0' }],
+    backfaceVisibility: 'hidden',
+    perspective: 1000
   },
   nav: {
     position: 'absolute',
